d10c2d293a6a42c280f890ef53cfef6a1cdcd9ef
Donghan Miao <donghan@yahoo-inc.com>
2016-04-07 14:29:06 -0500

    add navigator

---
+++ Views/MerchantListView.js
@@ -0,0 +1,113 @@
+import React, {
+  AppRegistry,
+  Component,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+var API_URL = 'http://10.74.90.221:1337/api/v1/merchant';
+var PAGE_SIZE = 25;
+var PARAMS = '?page_size=5&current_page=1';
+
+var REQUEST_URL = API_URL + PARAMS;
+
+class MerchantListView extends Component {
+
+   constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.merchants),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  render () {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMerchant}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading merchants...
+        </Text>
+      </View>
+    );
+  }
+  renderMerchant(merchant) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: merchant.img_url}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{merchant.name}</Text>
+          <Text style={styles.year}>{merchant.url}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 120,
+    height: 90,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+module.exports = MerchantListView;
--- index.ios.js
+++ index.ios.js
@@ -1,119 +1,116 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
-
-import React, {
+'use strict';
+var React = require('react-native');
+var {
   AppRegistry,
-  Component,
-  Image,
-  ListView,
   StyleSheet,
   Text,
   View,
-} from 'react-native';
+  Navigator,
+  TouchableOpacity,
+  Component,
+} = React;
 
-var API_URL = 'http://10.74.90.221:1337/api/v1/merchant';
-var PAGE_SIZE = 25;
-var PARAMS = '?page_size=5&current_page=1';
 
-var REQUEST_URL = API_URL + PARAMS;
+var LoginView = require('./Views/LoginView');
+var MerchantListView = require('./Views/MerchantListView');
+
+var SCREEN_WIDTH = require('Dimensions').get('window').width;
+var BaseConfig = Navigator.SceneConfigs.FloatFromRight;
+
+var CustomLeftToRightGesture = Object.assign({}, BaseConfig.gestures.pop, {
+  // Make it snap back really quickly after canceling pop
+  snapVelocity: 8,
+  // Make it so we can drag anywhere on the screen
+  edgeHitWidth: SCREEN_WIDTH,
+});
+
+var CustomSceneConfig = Object.assign({}, BaseConfig, {
+  // A very tighly wound spring will make this transition fast
+  springTension: 100,
+  springFriction: 1,
+  // Use our custom gesture defined above
+  gestures: {
+    pop: CustomLeftToRightGesture,
+  }
+});
+/*
+var PageOne = React.createClass({
+  _handlePress() {
+    this.props.navigator.push({id: 2,});
+  },
+
+  render() {
+    return (
+      <View style={[styles.container, {backgroundColor: 'green'}]}>
+        <Text style={styles.welcome}>Greetings!</Text>
+        <TouchableOpacity onPress={this._handlePress}>
+          <View style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'black'}}>
+            <Text style={styles.welcome}>Go to page two</Text>
+          </View>
+        </TouchableOpacity>
+       </View>
+    )
+  },
+});
+
+var PageTwo = React.createClass({
+  _handlePress() {
+    this.props.navigator.pop();
+  },
+
+  render() {
+    return (
+      <View style={[styles.container, {backgroundColor: 'purple'}]}>
+        <Text style={styles.welcome}>This is page two!</Text>
+        <TouchableOpacity onPress={this._handlePress}>
+          <View style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'black'}}>
+            <Text style={styles.welcome}>Go back</Text>
+          </View>
+        </TouchableOpacity>
+       </View>
+    )
+  },
+});
+
+*/
 
 class ChifanApp extends Component {
-
-   constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.merchants),
-          loaded: true,
-        });
-      })
-      .done();
-  }
-
-  render () {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
+  _renderScene(route, navigator) {
+    if (route.id === 1) {
+      return <LoginView navigator={navigator} />
+    } else if (route.id === 2) {
+      return <MerchantListView navigator={navigator} />
     }
-
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderMerchant}
-        style={styles.listView}
-      />
-    );
   }
 
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading merchants...
-        </Text>
-      </View>
-    );
+  _configureScene(route) {
+    return CustomSceneConfig;
   }
-  renderMerchant(merchant) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: merchant.img_url}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{merchant.name}</Text>
-          <Text style={styles.year}>{merchant.url}</Text>
-        </View>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
+  render() {
+    return (
+      <Navigator
+        initialRoute={{id: 1, }}
+        renderScene={this._renderScene}
+        configureScene={this._configureScene} />
+    );
+  }
+};
+
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
+  welcome: {
     fontSize: 20,
-    marginBottom: 8,
     textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  thumbnail: {
-    width: 120,
-    height: 90,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
+    margin: 10,
+    color: 'white',
   },
 });
+
 
 AppRegistry.registerComponent('chifan', () => ChifanApp);
