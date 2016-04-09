1ca24b126a828fb7011367a1f95dc19b3ff98904
Donghan Miao <donghan@yahoo-inc.com>
2016-04-08 23:02:33 -0500

    asd

---
+++ Views/RegisterView.js
@@ -0,0 +1,106 @@
+"use strict";
+
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import MerchantListView from './MerchantListView';
+
+var styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: "stretch"
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 10
+  },
+  formInput: {
+    height: 36,
+    padding: 10,
+    marginRight: 5,
+    marginBottom: 5,
+    marginTop: 5,
+    flex: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "#555555",
+    borderRadius: 8,
+    color: "#555555"
+  },
+  button: {
+    height: 40,
+    flex: 1,
+    backgroundColor: "#555555",
+    borderColor: "#555555",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginTop: 10,
+    justifyContent: "center"
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#ffffff",
+    alignSelf: "center"
+  },
+});
+
+class RegisterView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Create an account with Golden Club
+        </Text>
+        <View>
+          <TextInput
+            placeholder="Username"
+            onChange={(event) => this.setState({ username: event.nativeEvent.text }) }
+            style={styles.formInput}
+            value={this.state.username} />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry={true}
+            onChange={(event) => this.setState({ password: event.nativeEvent.text }) }
+            style={styles.formInput}
+            value={this.state.password} />
+           <TextInput
+            placeholder="Confirm Password"
+            secureTextEntry={true}
+            onChange={(event) => this.setState({ password: event.nativeEvent.text }) }
+            style={styles.formInput}
+            value={this.state.password2} />
+          <TouchableHighlight onPress={(this.onSubmitPressed.bind(this)) } style={styles.button}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+
+  onSubmitPressed() {
+   // this.props.navigator.push({
+   //   component: MerchantListView,
+   //   passProps: { username: this.state.username, password: this.state.password },
+   //  });
+  }
+};
+
+
+module.exports = RegisterView;
